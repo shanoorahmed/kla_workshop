55d961971e3e6517d55718df684746951914c8f7
shanoorahmed <shanoorahmed575@gmail.com>
2022-03-08 15:49:20 +0530

    milestone1b 100 score

--- Milestone1/main2.py
+++ Milestone1/main2.py
@@ -12,22 +12,18 @@ file1 = open("log2.txt","w")
 threads = []
 
 def flow(work, execution, activities):
+    now = datetime.now()
+    file1.write(f"{now};{work} Entry\n")
     if execution == "Sequential":
         for act in activities:
-            now = datetime.now()
-            file1.write(f"{now};{work}.{act} Entry\n")
             if activities[act]['Type'] == "Flow":
                 newwork = work + '.' + act
                 flow(newwork,activities[act]['Execution'],activities[act]['Activities'])
             elif activities[act]['Type'] == "Task":
                 newwork = work + '.' + act
                 task(newwork,activities[act]['Function'],activities[act]['Inputs'])
-            now = datetime.now()
-            file1.write(f"{now};{work}.{act} Exit\n")
     elif execution == "Concurrent":
         for act in activities:
-            now = datetime.now()
-            file1.write(f"{now};{work}.{act} Entry\n")
             if activities[act]['Type'] == "Flow":
                 newwork = work + '.' + act
                 thread = threading.Thread(target=flow,args=[newwork,activities[act]['Execution'],activities[act]['Activities']])
@@ -38,25 +34,28 @@ def flow(work, execution, activities):
                 thread = threading.Thread(target=task,args=[newwork,activities[act]['Function'],activities[act]['Inputs']])
                 thread.start()
                 threads.append([thread,newwork])
-        for thread in threads:
-            thread[0].join()
-            now = datetime.now()   
-            file1.write(f"{now};{thread[1]} Exit\n")
+        flag = 1
+        while flag == 1:
+            flag = 0
+            for thread in threads:
+                if thread[0].is_alive():
+                    flag = 1
+    now = datetime.now()
+    file1.write(f"{now};{work} Exit\n")
 
 def task(work,function,inputs):
     if function == 'TimeFunction':
         fun_input = inputs['FunctionInput']
         exc_time = inputs['ExecutionTime']
         now = datetime.now()
+        file1.write(f"{now};{work} Entry\n")
         file1.write(f"{now};{work} Executing {function} ({fun_input}, {exc_time})\n")
         time.sleep(int(exc_time))
+        now = datetime.now()
+        file1.write(f"{now};{work} Exit\n")
 
 for work in workflow:
-    now = datetime.now()
-    file1.write(f"{now};{work} Entry\n")
     if workflow[work]['Type'] == "Flow":
         flow(work,workflow[work]['Execution'],workflow[work]['Activities'])
     elif workflow[work]['Type'] == "Task":
         task(work,workflow[work]['Function'],workflow[work]['Inputs'])
-    now = datetime.now()
-    file1.write(f"{now};{work} Exit")
